a64622e7f753d0a97ced4dfc2581579029ad46c8
maydaysuper <140296215+maydaysuper@users.noreply.github.com>
2026-09-18 13:08:27 +0800

    Add Program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+namespace WardogsNavigator;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainForm());
+    }
+}
